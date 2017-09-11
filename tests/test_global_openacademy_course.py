20dbf6d06e3ea35b9da8b617bea32a07e2a98fa9
MauroR18 <mrm_1889@hotmail.com>
2017-09-11 18:11:08 -0500

    [REF] openacademy: Add duplicate test to openacademy course

--- tests/test_global_openacademy_course.py
+++ tests/test_global_openacademy_course.py
@@ -34,16 +34,43 @@ class GlobalTestOpenAcademyCourse(TransactionCase):
 
     # Mute the error odoo.sql_db to avoid it in log
     @mute_logger('odoo.sql_db')
-    def test_01_same_name_description(self):
-         """
-                Create a course with same name and description
-                to test constraint "name different of description"
-         """
-         # Error raised expected with message expected
-         with self.assertRaisesRegexp(
+    def test_10_same_name_description(self):
+        """
+            Create a course with same name and description
+            to raise constraint "name different of description"
+        """
+
+        # Error raised expected with message expected
+        with self.assertRaisesRegexp(
                  IntegrityError,
                  'new row for relation "openacademy_course" violates'
-                 ' check constraint "openacademy_course_name_description_check"'
+                 ' check constraint "openacademy_course_name_description_check"'):
+            # create a course with same name and description to raise an error
+            self.create_course('test','test',None)
+
+    @mute_logger('odoo.sql_db')
+    def test_20_two_courses_same_name(self):
+        """
+            Create two courses with the same name,
+            to raise constraint of unique name
+        """
+        new_id = self.create_course('test1', 'test_description', None)
+        print "new_id: ", new_id
+
+        with self.assertRaisesRegexp(
+                 IntegrityError,
+                 'duplicate key value violates unique'
+                 ' constraint "openacademy_course_name_unique"'
                 ):
-             # create a course with same name and description to raise an error
-             self.create_course('test','test',None)
+            new_id2 = self.create_course('test1', 'test_description', None)
+            print "new_id2: ", new_id2
+
+    def test_15_duplicate_course(self):
+        """
+            Duplicate a course and check that work fine!
+        """
+
+        course = self.env.ref('openacademy.course0')
+        course_id = course.copy()
+
+        print "copy_course_id: ", course_id
